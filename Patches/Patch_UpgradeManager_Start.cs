ac1b9b5d68294140205b6bd86acdbb725e40e650
Jack de Wilde <Jackdew31@icloud.com>
2022-01-31 18:40:03 +1000

    Initial Release

---
+++ Patches/Patch_UpgradeManager_Start.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Respec.Patches
+{
+    [HarmonyPatch(typeof(UpgradeManager), "Start")]
+    internal class Patch_UpgradeManager_Start
+    {
+		[HarmonyPostfix]
+        public static void Postfix(UpgradeManager __instance)
+        {
+            var resetButton = GameObject.Find("Canvas/UpgradeMenu/ResetButton(DEBUG)").GetComponent<Button>();
+            resetButton.onClick = new Button.ButtonClickedEvent();
+            resetButton.onClick.AddListener(new UnityEngine.Events.UnityAction(__instance.ResetUpgrades));
+            resetButton.GetComponentInChildren<Text>().text = "Respec XP";
+        }
+    }
+}
+
---
+++ Respec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+namespace Respec
+{
+    public class Respec : MelonMod
+    {
+        public const string modVersion = "1.0.0";
+        public const string modName = "Respec";
+    }
+}
