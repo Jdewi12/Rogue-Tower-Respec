d41d724a2168239beca5ec143fe5e8ef2340caf3
Jack de Wilde <Jackdew31@icloud.com>
2022-02-10 00:21:45 +1000

    BepInEx
    Fix Harmony patches

--- Respec.cs
+++ Respec.cs
@@ -1,7 +1,15 @@
 using BepInEx;
+using HarmonyLib;
 
 namespace Respec
 {
     [BepInPlugin("Jdewi.Respec", "Respec", "1.0.0")]
-    public class Respec : BaseUnityPlugin { }
+    public class Respec : BaseUnityPlugin 
+    {
+        void Awake()
+        {
+            var instance = new Harmony("Jdewi.Respec");
+            instance.PatchAll();
+        }
+    }
 }
