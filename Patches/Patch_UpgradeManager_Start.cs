df706eda308299d5997b61872e8f90268c7995a9
Jack de Wilde <Jackdew31@icloud.com>
2022-02-11 12:40:25 +1000

    Fix for 1.08

--- Patches/Patch_UpgradeManager_Start.cs
+++ Patches/Patch_UpgradeManager_Start.cs
@@ -10,7 +10,7 @@ namespace Respec.Patches
         [HarmonyPostfix]
         public static void Postfix(UpgradeManager __instance)
         {
-            var resetButton = GameObject.Find("Canvas/UpgradeMenu/ResetButton(DEBUG)").GetComponent<Button>();
+            var resetButton = GameObject.Find("Canvas/UpgradeMenu/Scaler/ResetButton(DEBUG)").GetComponent<Button>();
             resetButton.onClick = new Button.ButtonClickedEvent();
             resetButton.onClick.AddListener(new UnityEngine.Events.UnityAction(__instance.ResetUpgrades));
             resetButton.GetComponentInChildren<Text>().text = "Respec XP";
